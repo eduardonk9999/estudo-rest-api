7370f162c34da64a312434f329526e84430db3f1
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-10 13:47:08 -0300

    Add, get

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
 
 app.get("/clients", function(req, res){
-  
+  res.json(data)
 });
 app.post("/clients/:id", function(req, res){
-  
+
 });
 app.put("/clients/:id", function(req, res){
-  
+
 });
 app.delete("/clients/:id", function(req, res){
-  
+
 });
 
 app.listen('3000', function(){
