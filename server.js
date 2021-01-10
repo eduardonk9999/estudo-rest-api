00ae4de0cd12d6d34caa0f3c19df913be137dee6
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-10 13:42:25 -0300

    Add. dataajson users

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.get("/clients", function(req, res){
+  
+});
+app.post("/clients/:id", function(req, res){
+  
+});
+app.put("/clients/:id", function(req, res){
+  
+});
+app.delete("/clients/:id", function(req, res){
+  
+});
+
 app.listen('3000', function(){
   console.log('Opá');
 });
