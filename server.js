5f29a885e68b6ce5cc500ae8621cd1fa25e79eba
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-10 14:02:24 -0300

    Add, final

--- server.js
+++ server.js
@@ -21,10 +21,21 @@ app.post("/clients", function(req, res){
   res.json({ name, email });
 });
 app.put("/clients/:id", function(req, res){
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if(!client) return res.status(204).json();
+  const { name } = req.body;
+
+  client.name = name;
+  res.json(client);
 
 });
 app.delete("/clients/:id", function(req, res){
+  const { id } = req.params;
+  const clientsFiltered = data.filter(client => client.id != id);
 
+  res.json(clientsFiltered);
 });
 
 app.listen('3000', function(){
