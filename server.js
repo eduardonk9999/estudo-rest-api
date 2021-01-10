3ca9a0fc7458177942e10bd39ae497a33a605511
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-10 13:56:33 -0300

    Add, mais rotas

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ app.use(express.json());
 app.get("/clients/:id", function(req, res){
   const { id } = req.params;
   const client = data.find(cli => cli.id == id);
+
+  if(!client) return res.status(204).json();
+
+  res.json(client);
 });
 app.get("/clients", function(req, res){
   res.json(data)
 });
-app.post("/clients/:id", function(req, res){
+app.post("/clients", function(req, res){
+  const { name, email } = req.body;
 
+  res.json({ name, email });
 });
 app.put("/clients/:id", function(req, res){
 
